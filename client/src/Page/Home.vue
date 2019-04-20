ea8747477ba55a56b7bcd575bc90cca15889da80
Icyscools <meet.ice1@gmail.com>
2019-04-20 14:28:52 +0700

    Re-Structure Vue

---
+++ client/src/Page/Home.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="home">
+		<h1>Home</h1>
+	</div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data() {
+    return {
+      msg: '',
+    };
+  }
+};
+</script>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+import Home from '@/Page/Home';
 import Ping from '@/components/Ping';
 
 Vue.use(Router);
@@ -10,8 +10,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld,
+      name: 'Home',
+      component: Home,
     },
     {
       path: '/ping',
