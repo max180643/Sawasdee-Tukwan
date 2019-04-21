8330bb66585311df74e8b517d805da07ab822e9a
Dwakez <dwakez250@gmail.com>
2019-04-21 12:40:26 +0700

    Fix Indent

--- server/app.py
+++ server/app.py
@@ -25,9 +25,9 @@ CORS(app)
 
 # to use it when loading the model
 def auc(y_true, y_pred):
-	auc = tf.metrics.auc(y_true, y_pred)[1]
-	keras.backend.get_session().run(tf.local_variables_initializer())
-	return auc
+    auc = tf.metrics.auc(y_true, y_pred)[1]
+    keras.backend.get_session().run(tf.local_variables_initializer())
+    return auc
 
 # load the model, and pass in the custom metric function
 global graph, model
@@ -41,61 +41,61 @@ def ping_pong():
 
 @app.route('/api', methods=['GET', 'POST'])
 def api_version():
-	return jsonify(name="SawandeeTukwan",
-				   version="1.0.0")
+    return jsonify(name="SawandeeTukwan",
+                   version="1.0.0")
 
 @app.route('/api/randomImage', methods=['GET'])
 def randomImage():
-	day = datetime.datetime.today().weekday() + 1
-	if (day == 7):
-		day = 0
+    day = datetime.datetime.today().weekday() + 1
+    if (day == 7):
+        day = 0
 
-	size = request.args.get('size')
-	encode = request.args.get('encode') if request.args.get('encode') else 'jpeg'
-	url = "https://loremflickr.com/%s/%s/flower" % (size, size)
-	image_obj = GenerateImage(url, size, day)
-	image_obj.addText()
-	image_de = serveImage(image_obj.img, encode)
-	return jsonify(base64=base64.b64encode(image_de).decode('utf-8'),
-				   type=encode,
-				   size=[size, size])
+    size = request.args.get('size')
+    encode = request.args.get('encode') if request.args.get('encode') else 'jpeg'
+    url = "https://loremflickr.com/%s/%s/flower" % (size, size)
+    image_obj = GenerateImage(url, size, day)
+    image_obj.addText()
+    image_de = serveImage(image_obj.img, encode)
+    return jsonify(base64=base64.b64encode(image_de).decode('utf-8'),
+                   type=encode,
+                   size=[size, size])
 
 @app.route('/api/customImage', methods=['GET'])
 def customImage():
-	day = datetime.datetime.today().weekday() + 1
-	if (day == 7):
-		day = 0
+    day = datetime.datetime.today().weekday() + 1
+    if (day == 7):
+        day = 0
 
-	msg = request.args.get('msg')
-	size = request.args.get('size')
-	encode = request.args.get('encode') if request.args.get('encode') else 'jpeg'
-	url = "https://loremflickr.com/%s/%s/flower" % (size, size)
-	image_obj = GenerateImage(url, size, day)
-	image_obj.addText(msg)
-	image_de = serveImage(image_obj.img, encode)
-	return jsonify(base64=base64.b64encode(image_de).decode('utf-8'),
-				   type=encode,
-				   size=[size, size])
+    msg = request.args.get('msg')
+    size = request.args.get('size')
+    encode = request.args.get('encode') if request.args.get('encode') else 'jpeg'
+    url = "https://loremflickr.com/%s/%s/flower" % (size, size)
+    image_obj = GenerateImage(url, size, day)
+    image_obj.addText(msg)
+    image_de = serveImage(image_obj.img, encode)
+    return jsonify(base64=base64.b64encode(image_de).decode('utf-8'),
+                   type=encode,
+                   size=[size, size])
 
 @app.route("/api/predictImage", methods=["GET","POST"])
 def predict():
-	size = request.args.get('size')
-	encode = request.args.get('encode') if request.args.get('encode') else 'jpeg'
-	label_arr = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-	img = GenerateImage("https://loremflickr.com/%s/%s/flower" % (size, size), size)
-	data = img.img.getdata()
-	data = np.array(data).reshape(-1, 500, 500, 3)
-	result = model.predict(data.reshape(-1, 500, 500, 3))
-	#result_class = label_arr[np.argmax(result)]
-	img.setDay(np.argmax(result))
-	img.addText()
-	return jsonify(base64=base64.b64encode(image_de).decode('utf-8'),
-				   type=encode,
-				   size=[size, size])
+    size = request.args.get('size')
+    encode = request.args.get('encode') if request.args.get('encode') else 'jpeg'
+    label_arr = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    img = GenerateImage("https://loremflickr.com/%s/%s/flower" % (size, size), size)
+    data = img.img.getdata()
+    data = np.array(data).reshape(-1, 500, 500, 3)
+    result = model.predict(data.reshape(-1, 500, 500, 3))
+    #result_class = label_arr[np.argmax(result)]
+    img.setDay(np.argmax(result))
+    img.addText()
+    return jsonify(base64=base64.b64encode(image_de).decode('utf-8'),
+                   type=encode,
+                   size=[size, size])
 
 @app.route("/api/getRandomText", methods=["GET", "POST"])
 def getText():
-	datasetpath = "../csv/" + str(random.randint(0, 7)) + ".csv"
+    datasetpath = "../csv/" + str(random.randint(0, 7)) + ".csv"
     with open(datasetpath, encoding="utf-8") as f:
         reader = csv.reader(f)
         low_text = choice(list(reader)) if not msg else msg
