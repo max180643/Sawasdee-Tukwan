1b41033f6ac477445681ae7581f15f01ae1a110d
Icyscools <meet.ice1@gmail.com>
2019-04-21 11:20:44 +0700

    Add margin app

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <Navbar></Navbar>
-    <router-view/>
+    <div id="content">
+      <router-view/>
+    </div>
   </div>
 </template>
 
@@ -26,5 +28,8 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+#content {
+  margin-top: 20px;
 }
 </style>
